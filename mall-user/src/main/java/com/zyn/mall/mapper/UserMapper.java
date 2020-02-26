0f2a6794f7ee14272d3b7e32fde3ded501c9652b
PhoebeNan <13037650105@163.com>
2020-02-26 22:01:33 +0800

    mall-user 2

--- mall-user/src/main/java/com/zyn/mall/mapper/UserMapper.java
+++ mall-user/src/main/java/com/zyn/mall/mapper/UserMapper.java
@@ -4,12 +4,14 @@ package com.zyn.mall.mapper;
 import com.zyn.mall.user.UmsMember;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 /**
  * @author zhaoyanan
  * @create 2019-10-30-9:15
  */
 public interface UserMapper extends Mapper<UmsMember> {
 
-    //List<UmsMember> selectAllUser();
+    List<UmsMember> selectAllUser();
 
 }
